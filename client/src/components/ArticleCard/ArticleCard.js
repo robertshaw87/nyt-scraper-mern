0e868672556818275f724c526b6b4af3364d123c
jimmytu <jimmytutron@gmail.com>
2018-06-26 15:01:36 -0700

    Create article card

--- client/src/components/ArticleCard/ArticleCard.js
+++ client/src/components/ArticleCard/ArticleCard.js
@@ -2,10 +2,44 @@ import React from "react";
 import "./ArticleCard.css";
 
 const ArticleCard = props => (
-  <div>
+<div className="card">
+  <div className="card-header text-right">
+        
+        {
 
+    	(window.location.pathname === "/" || window.location.pathname === "/search")  
+    	?
+    		<button 
+	    		className="btn btn-primary" 
+	    		disabled = {props.alreadySaved}
+	    		onClicked = {props.save}
+    		>
+	    		{(props.alreadySaved 
+	    			? <i class="fas fa-bookmark"></i> 
+	    			: <i class="far fa-bookmark"></i>)}
+    		</button>
+    	:
+    		<button
+	    		className="btn btn-danger"
+	    		onClicked = {props.delete}
+    		>
+    			<i class="fas fa-trash-alt"></i>
+    		</button>
+    	
+    	}
+    	
+  </div>
+  <div className="card-body">
+  	{props.img 
+  		? <img className="float-left" src={props.img} alt={props.title}/> 
+  		: null}
+    <h5 className="card-title">
+    <a href={props.url} target="_blank">{props.title}</a>
+    </h5>
+    <p className="card-text">{props.description}</p>
 
   </div>
+</div>
 );
 
 export default ArticleCard;
