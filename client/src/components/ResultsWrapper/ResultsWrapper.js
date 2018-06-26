78f6a93db0ef6f1721df38f7a95e277cbe35186f
Robert Shaw <robertshaw87@gmail.com>
2018-06-26 11:19:38 -0700

    create front-end skeleton (maybe done?)

---
+++ client/src/components/ResultsWrapper/ResultsWrapper.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./ResultsWrapper.css";
+
+const ResultsWrapper = () => (
+  <div>
+
+    
+  </div>
+);
+
+export default ResultsWrapper;
---
+++ client/src/components/SavedArticleWrapper/SavedArticleWrapper.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./SavedArticleWrapper.css";
+
+const SavedArticleWrapper = () => (
+  <div>
+
+    
+  </div>
+);
+
+export default SavedArticleWrapper;
