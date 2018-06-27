086fe619f4442466e7efc413de2d5cacd347d2c7
Robert Shaw <robertshaw87@gmail.com>
2018-06-26 17:22:33 -0700

    add handler for empty search result

--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -11,7 +11,8 @@ class Search extends Component {
     savedArticles: [],
     topic: "",
     start: "",
-    end: ""
+    end: "",
+    emptySearch: false
   };
 
   componentDidMount() {
@@ -49,11 +50,13 @@ class Search extends Component {
 
   		API.searchArticles(topic, start, end)
   		.then(res => {
-        console.log("=======================")
-        console.log(res.data)
-        console.log("=======================")
-        return this.setState({articles: res.data, topic:"", start:"", end:""})}
-      )
+        let articles = res.data;
+        let emptySearch = false;
+        if (articles.length <= 0) {
+          emptySearch = true;
+        }
+        this.setState({articles, emptySearch, topic:"", start:"", end:""})
+      })
   		.catch(err => console.log(err));
 
   	};
@@ -112,7 +115,11 @@ class Search extends Component {
 	  		</Row>
         <Row className="justify-content-center">
           <Col size="10">
-            {this.state.articles.map((article, i) => (
+            {this.state.emptySearch
+            ?
+            <h3>No results found. Please try another query.</h3>
+            :
+              this.state.articles.map((article, i) => (
               <ArticleCard 
                 title = {article.title}
                 description = {article.description}
@@ -121,8 +128,8 @@ class Search extends Component {
                 save = {() => this.saveArticle(i)}
                 alreadySaved = {this.checkSaved(article)}
                 key = {i}
-              />
-            ))}
+              />))
+            }
           </Col>
         </Row>
 
